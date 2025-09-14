042d5c37f52fb930e4f5621a8d8713b0ffb5de25
JAbdullah-9T <abdullah.jr@obmontessori.edu.ph>
2025-09-14 16:30:03 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# PYTHON
+from pyscript import display
+
+
+restaurant_name = 'Hot to Go!' #string
+owner_name = 'Jalainie R. Abdullah Jr.' #string
+year_established = '2025' #integer
+popular_item_price = '200' #integer
+has_delivery = True   # boolean delivery is available
+print(has_delivery)   # Output: True
+has_delivery = False  # boolean delivery is NOT available
+print(has_delivery)   # Output: False
+product_names = ['Pizza', 'Burger', 'Hotdog'] #list
+business_hours = (opentime(10:00), closetime(10:00)) #tuple
+weight = '56.09' #float
+display(f'My name is {first_name} {last_name}', target='div1')
+display(f'I am a part of 10 {sections}')
